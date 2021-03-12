b5d1a17e210e89cab7f9d42314a2dbcc3ffb687f
Alexey Pargachev <pargachev20@gmail.com>
2021-03-12 15:47:35 +0700

    add files

---
+++ script.js
@@ -0,0 +1,20 @@
+let a, b, func;
+
+function Pryam() {
+  a = document.getElementById('a').value;
+  b = document.getElementById('b').value;
+  func = document.getElementById('func').value;
+  
+}
+
+function Trap() {
+  a = document.getElementById('a').value;
+  b = document.getElementById('b').value;
+  func = document.getElementById('func').value;
+}
+
+function Simpson() {
+  a = document.getElementById('a').value;
+  b = document.getElementById('b').value;
+  func = document.getElementById('func').value;
+}
